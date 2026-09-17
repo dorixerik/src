8b66e1069a2aec1481126e404a363bc144f90fd9
mahayov <doregore21@gmail.com>
2026-09-17 21:22:28 +0300

    first commit

---
+++ Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level01.task03;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+      
+        int gh = 2;
+        System.out.print(gh);
+    }
+    {
+        int gh = 1;
+        System.out.print(gh);
+    }
+}
